641364484df84404bb0c50009497eb353ac40d87
david manukyan <david19manukyan@gmail.com>
2022-12-15 19:27:09 +0300

    add some Example

---
+++ src/chepter2/Fortest.java
@@ -0,0 +1,12 @@
+package chepter2;
+
+public class Fortest {
+    public static void main(String[] args) {
+
+        for (int a = 0; a < 10; a++) {
+            System.out.println(a);
+        }
+    }
+
+
+}
---
+++ src/chepter2/Varexample.java
@@ -0,0 +1,19 @@
+package chepter2;
+
+public class Varexample {
+    public static void main(String[] args) {
+        boolean b;
+        b = false;
+        System.out.println("b =" + b);
+        b = true;
+        System.out.println(" b =" + b);
+
+        if (b)
+            System.out.println("kereva");
+        b = false;
+
+
+    }
+
+
+}
